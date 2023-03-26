b0aec4389919579ef0da18eb5676e7371b872aaf
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-27 02:39:43 +0300

    .

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const INFURA_URL = process.env.INFURA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // goerli: {
+    //   url: INFURA_URL,
+    //   accounts: [PRIVATE_KEY],
+    // },
+    mumbai: {
+      url: process.env.POKT_MUMBAI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
