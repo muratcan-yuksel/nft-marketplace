e58843808c6cca1fa9683bd50cd72e1b0d9ff156
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-27 06:19:15 +0300

    add navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <p className="mr-6 text-pink-500">Home</p>
+          </Link>
+          <Link href="/create-item">
+            <p className="mr-6 text-pink-500">Sell Digital Asset</p>
+          </Link>
+          <Link href="/my-assets">
+            <p className="mr-6 text-pink-500">My Digital Assets</p>
+          </Link>
+          <Link href="/creator-dashboard">
+            <p className="mr-6 text-pink-500">Creator Dashboard</p>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
