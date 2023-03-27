4613afa116b8525893cfbb9da356d6359697a8c4
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-27 04:26:26 +0300

    .

---
+++ contracts/NFT.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    //counters helps us in incrementing
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    address contractAddress;
+
+    constructor(
+        address _marketplaceAddress
+    ) ERC721("Metaverse Tokens", "METT") {
+        contractAddress = _marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        //for instance this function is coming from ERC721URIStorage
+        _setTokenURI(newItemId, tokenURI);
+
+        setApprovalForAll(contractAddress, true);
+
+        return newItemId;
+    }
+}
